c69508956e72643972881a94990c3573662ee17f
syafiqparadisam <syafiqparadisam@gmail.com>
2024-12-20 20:13:13 +0700

    walpaper ngentekno memory

--- .config/hypr/set_wallpaper.sh
+++ .config/hypr/set_wallpaper.sh
@@ -5,6 +5,6 @@ IMAGE_PATH=$1  # Ambil parameter pertama (file gambar)
 MONITOR="eDP-1"  # Ganti dengan monitor yang sesuai jika diperlukan
 
 # Periksa apakah file ada
-#    hyprctl hyprpaper preload "$HOME/backgrounds/$IMAGE_PATH" && 
+    hyprctl hyprpaper preload "$HOME/backgrounds/$IMAGE_PATH" && 
 	    hyprctl hyprpaper wallpaper "$MONITOR,$HOME/backgrounds/$IMAGE_PATH"
 
